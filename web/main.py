128a6a986460b001d43891d098ed228e6ed8d70b
Ida Bergquist <idabe112@student.liu.se>
2018-10-04 11:15:27 +0200

    Added data.py and some stuff to main.py

--- web/main.py
+++ web/main.py
@@ -1,23 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+import data
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    pass
+    return render_template("index.html")
 
 @app.route("/search")
 def search():
-    pass
+    return render_template("search.html")
 
 @app.route("/list")
 def listpage():
-    pass
+    return render_template("list.html")
 
-@app.route("/projects")
+@app.route("/project/<project_id>")
 def projects():
-    pass
+    return render_template("project.html", project_id=project_id)
 
 if __name__ == "__main__":
     app.run(debug=True)
