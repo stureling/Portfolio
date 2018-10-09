f7293662efa19862ef3385edd05bad7d9401b92d
Ida Bergquist <idabe112@student.liu.se>
2018-10-09 22:18:06 +0200

    Added login functionality with Flask-Login and message flashing.

--- web/main.py
+++ web/main.py
@@ -2,20 +2,36 @@ from flask import Flask, render_template, url_for, request, redirect
 from flask import session, abort, flash, escape
 import data
 import os
+from flask_login import LoginManager, login_user, login_required, UserMixin
+from flask_login import logout_user
 
-
+# Start Flask
 app = Flask(__name__)
 app.secret_key = os.urandom(64)
 db = data.load("data.json")
 
+
+# User class for Flask-Login
+class User(UserMixin):
+    def __init__(self, user_id):
+        self.id = user_id
+        
 # Database file of all the users and their passwords
 users = data.load_users("users.json")
+    
+# Start Flask-Login
+login_manager = LoginManager()
+login_manager.init_app(app)
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User(user_id)
+
+
+# Start Flask routes
 @app.route("/bootstrap")
 def bootstrap():
-
     projects = data.search(db, search="")
-
     return render_template("index_bootstrap2.html", **locals())
 
 @app.route("/")
@@ -24,7 +40,8 @@ def index():
 
 @app.route("/techniques")
 def techniques():
-    """Creates and sorts a list of all project by the technique used. Then it displays a page of all projects"""
+    """Creates and sorts a list of all project by the technique used.
+    Then it displays a page of all projects"""
     master_list = []
     technique_list = data.get_techniques(db)
     for e in technique_list:
@@ -39,7 +56,8 @@ def techniques():
 
 @app.route("/list", methods=["POST", "GET"])
 def list():
-    """Creates a page where you can search the database for projects and sort them"""
+    """Creates a page where you can search the database
+    for projects and sort them"""
     technique_data = data.get_techniques(db)
     search_fields = data.get_searchfields(db)
 
@@ -62,24 +80,40 @@ def project(project_id):
                                             int(project_id)))
 
 @app.route("/add")
+@login_required
 def add():
-    if "logged_in" not in session:
-        abort(401)
     global db
-    return "Logged in as",  escape(session.get("logged_in"))
     return render_template("add.html", **locals())
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    error = None
+    authorized = False
     if request.method == "POST":
+        # Check if user is in JSON DB.
         for user in users:
-            if (request.form["username"] == user["username"]
-                and request.form["password"] == user["password"]):
-                session["logged_in"] = request.form["username"]
+            username = request.form["username"]
+            password = request.form["password"]
+            if (username == user["username"]
+                and password == user["password"]):
+                # Handle the login
+                username = User(username)
+                login_user(username)
+                authorized = True
                 break
+
+        if not authorized:
+            flash("Invalid login, please try again.", "error")
+        elif authorized:
+            flash("Login succesful.", "success")
+            return redirect(url_for("add"))
         
-    return render_template("login.html", error=error)
+    return render_template("login.html")
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return "Logged ya out, brosef."
     
 @app.errorhandler(404)
 def page_not_found(error):
@@ -90,4 +124,3 @@ def invalid_login(error):
     return render_template("access_denied.html"), 401
 if __name__ == "__main__":
     app.run(debug=True)
-
